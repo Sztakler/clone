8e7444a853650fee5b2a007772af766dfa4d758b
metarth <krystian.jasionek@gmail.com>
2025-04-06 18:37:20 +0200

    Add function to format uptime as string

--- backend/app/utils/time_utils.py
+++ backend/app/utils/time_utils.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import timedelta
 
 def to_uint32(value: float, max_value: Optional[int] = None) -> int:
     """
@@ -14,3 +15,18 @@ def to_uint32(value: float, max_value: Optional[int] = None) -> int:
     max_val = max_value if max_value is not None else (2**32 - 1)
 
     return int(value) % max_val
+
+def format_uptime(seconds: int) -> str:
+    """
+    Format the uptime from seconds to a human-readable string.
+
+    Args:
+        seconds (int): The uptime in seconds.
+
+    Returns:
+        str: A string representing the uptime in a human-readable format.
+             For example, '1 day, 2 hours, 3 minutes, 4 seconds'.
+    """ 
+    uptime = timedelta(seconds = seconds)
+
+    return str(uptime)
