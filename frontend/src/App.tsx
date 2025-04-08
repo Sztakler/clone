a63eb8782b43ad26c32d5d99a9e46a658053c884
Metarth <58264262+Sztakler@users.noreply.github.com>
2025-04-08 18:20:35 +0200

    17 frontend create interface for robot control (#24)
    * Implement interface for robot controls
    
    * Add better styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import RobotState from './components/RobotState';
+import RobotControls from './components/RobotControls'; 
 
 function App() {
   return (
     <div className="App">
       <RobotState />
+      <RobotControls />
     </div>
   );
 }
