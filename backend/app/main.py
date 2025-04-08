43eb788a52ce031cad031ebd903486edb5fa5372
Metarth <58264262+Sztakler@users.noreply.github.com>
2025-04-08 15:11:22 +0200

    21 backend configure backend for cors (#22)
    * Install fastapi CORS middleware
    
    * Add frontend server to origins and configure CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from utils.logging import configure_logging, LogLevel
 from services.robot_service import RobotService, robot_service
@@ -9,6 +10,18 @@ from pydantic import ValidationError
 from websockethub import WebSocketHub
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 configure_logging(log_level=LogLevel.DEBUG)
 
